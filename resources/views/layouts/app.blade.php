2315b5e7d24084f274b311e57caa0d5527bb2438
faiirusss <87980888+faiirusss@users.noreply.github.com>
2024-06-09 15:05:21 +0700

    update layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,20 +17,54 @@
 </head>
 
 <body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100">
-        <livewire:layout.navigation />
-
-        <!-- Page Heading -->
-        @if (isset($header))
-        <header class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header }}
+    <div class="min-h-screen bg-white">
+        <div class="grid grid-cols-1 divide-x lg:grid-cols-3">
+            <div class="flex flex-col h-32 p-5 bg-white divide-y divide-gray-300 lg:col-span-2">
+                <div class="flex items-center space-x-7">
+                    <div class="flex items-center w-full divide-x divide-gray-300">
+                        <button type="submit" class="flex-shrink-0 pr-4">
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
+                                    d="M5 7h14M5 12h14M5 17h14" />
+                            </svg>
+                        </button>
+                        <form class="flex items-center w-full pl-4">
+                            <label for="simple-search" class="sr-only">Search</label>
+                            <div class="relative w-full">
+                                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
+                                            d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" />
+                                    </svg>
+                                </div>
+                                <input type="text" id="simple-search"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="Search branch name..." required />
+                            </div>
+                            <button type="submit"
+                                class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                    viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="M5 12h14m-7 7V5" />
+                                </svg>
+                                <span class="sr-only">Search</span>
+                            </button>
+                        </form>
+                    </div>
+                </div>
+                <!-- Elemen berikut akan berada di bawah pembatas horizontal -->
+                <div class="flex items-center pt-4 space-x-7">
+                    <!-- Tambahkan konten di sini jika diperlukan -->
+                </div>
             </div>
-        </header>
-        @endif
+            <div class="h-32 bg-white"></div>
+        </div>
 
         <!-- Page Content -->
-        <main>
+        <main class="">
             {{ $slot }}
         </main>
     </div>
--- resources/views/livewire/cart/order.blade.php
+++ resources/views/livewire/cart/order.blade.php
@@ -1,48 +1,11 @@
 <div>
-    <x-slot name="header">
-        <ol
-            class="flex items-center w-full p-3 space-x-2 text-sm font-medium text-center text-gray-500 bg-white border border-gray-200 rounded-lg shadow-sm dark:text-gray-400 sm:text-base dark:bg-gray-800 dark:border-gray-700 sm:p-4 sm:space-x-4 rtl:space-x-reverse">
-            <li class="flex items-center text-blue-600 dark:text-blue-500">
-                <span
-                    class="flex items-center justify-center w-5 h-5 me-2 text-xs border border-blue-600 rounded-full shrink-0 dark:border-blue-500">
-                    1
-                </span>
-                Order <span class="hidden sm:inline-flex sm:ms-2">Info</span>
-                <svg class="w-3 h-3 ms-2 sm:ms-4 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                    fill="none" viewBox="0 0 12 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="m7 9 4-4-4-4M1 9l4-4-4-4" />
-                </svg>
-            </li>
-            <li class="flex items-center">
-                <span
-                    class="flex items-center justify-center w-5 h-5 me-2 text-xs border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
-                    2
-                </span>
-                Payment <span class="hidden sm:inline-flex sm:ms-2">Method</span>
-                <svg class="w-3 h-3 ms-2 sm:ms-4 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                    fill="none" viewBox="0 0 12 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="m7 9 4-4-4-4M1 9l4-4-4-4" />
-                </svg>
-            </li>
-            <li class="flex items-center">
-                <span
-                    class="flex items-center justify-center w-5 h-5 me-2 text-xs border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
-                    3
-                </span>
-                Finish
-            </li>
-        </ol>
-    </x-slot>
-
     <div class="py-12">
-        <div class="max-w-7xl sm:px-6 lg:px-8 py-3 mx-auto">
+        <div class="py-3 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <form class="max-w-md" wire:submit="createOrder">
                 <label for="default-search"
                     class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                 <div class="relative">
-                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <div class="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
                         <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -50,7 +13,7 @@
                         </svg>
                     </div>
                     <input type="text" id="default-search" wire:model="search"
-                        class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        class="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg ps-10 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="Search Product..." required />
                     <button" type="submit"
                         class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -59,8 +22,8 @@
             </form>
         </div>
 
-        {{-- <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-5">
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {{-- <div class="py-5 mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
                 @foreach ($products as $item)
                 <div wire:click="updateCart('{{ $item->id }}')"
                     class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
@@ -76,10 +39,10 @@
             </div>
         </div> --}}
 
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <table class="w-full text-sm text-left text-gray-500 rtl:text-right dark:text-gray-400">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
                                 {{-- <th scope="col" class="px-16 py-3">
@@ -106,17 +69,17 @@
                                 class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                                 {{-- <td class="p-4">
                                     <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png"
-                                        class="w-16 md:w-32 max-w-full max-h-full rounded-lg" alt="Apple Watch">
+                                        class="w-16 max-w-full max-h-full rounded-lg md:w-32" alt="Apple Watch">
                                 </td> --}}
                                 <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
                                     {{-- {{ $product->nama_produk }} --}}
                                     {{ $item->product->nama_produk }}
                                 </td>
-                                <td class=" px-6 py-4">
+                                <td class="px-6 py-4 ">
                                     {{-- {{ $item->quantity }} --}}
                                     <div class="flex items-center">
                                         <button wire:click="updateCart(false, '{{ $item->product->id }}')"
-                                            class="inline-flex items-center justify-center p-1 me-3 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                                            class="inline-flex items-center justify-center w-6 h-6 p-1 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full me-3 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
                                             type="button">
                                             <span class="sr-only">Quantity button</span>
                                             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
@@ -131,7 +94,7 @@
                                                 value="{{ $item->quantity }}" required />
                                         </div>
                                         <button wire:click="updateCart(true, '{{ $item->product->id }}')"
-                                            class="inline-flex items-center justify-center h-6 w-6 p-1 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                                            class="inline-flex items-center justify-center w-6 h-6 p-1 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full ms-3 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
                                             type="button">
                                             <span class="sr-only">Quantity button</span>
                                             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
@@ -171,7 +134,7 @@
                             </tr>
                         </tfoot> --}}
                     </table>
-                    <div class="px-6 py-4 flex justify-end">
+                    <div class="flex justify-end px-6 py-4">
                         <a href="/payment" wire:navigate
                             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             Checkout
@@ -187,7 +150,7 @@
 
             </div>
         </div>
-        <div class="max-w-7xl sm:px-6 lg:px-8 py-3 mx-auto">
+        <div class="py-3 mx-auto max-w-7xl sm:px-6 lg:px-8">
 
         </div>
     </div>
