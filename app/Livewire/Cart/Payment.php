4cf8feca2548cc3a4e2acc58e98e2b94b5d2abba
faiirusss <87980888+faiirusss@users.noreply.github.com>
2024-08-26 23:11:24 +0700

    update: payment cash

--- app/Livewire/Cart/Payment.php
+++ app/Livewire/Cart/Payment.php
@@ -86,6 +86,130 @@ class Payment extends Component
 
     }
 
+    // public function pay_cash()
+    // {
+    //     $this->order = Order::where('done_at', null)
+    //         ->with('orderProducts')
+    //         ->latest()
+    //         ->first();
+
+    //     $this->order->update([
+    //         'done_at' => now()
+    //     ]);
+
+    //     $orderId = strtoupper($this->order->invoice_number);
+    //     $cashier = auth()->user()->name;
+    //     $member = $this->order->member->name ?? $this->order->member->phone;
+    //     $address = 'Jl. Binong Jati No.124';
+    //     $address2 = 'Kec.Batununggal, Bandung, 40275';
+    //     $phone = '0856-2401-0106';
+    //     $date = $this->order->created_at->format('Y-m-d H:i');
+    //     $pembayaran = $this->order->payment_method;
+    //     $bayar = $this->order->paid_amount;
+    //     $kembali = $this->order->return_amount;
+
+    //     function centerTextCash($text, $width = 32) {
+    //         if (strlen($text) >= $width) {
+    //             return $text;
+    //         }
+    //         $padding = floor(($width - strlen($text)) / 2);
+    //         return str_repeat(' ', $padding) . $text;
+    //     }
+
+    //     // Format the receipt text
+    //     $text = centerTextCash('Merajut Asa Kita') . "\n";
+    //     $text .= centerTextCash($address) . "\n";
+    //     $text .= centerTextCash($address2) . "\n";
+    //     $text .= centerTextCash($phone) . "\n\n";
+
+    //     $text .= "Tanggal   : $date\n";
+    //     $text .= "Order ID  : $orderId\n";
+    //     $text .= "Kasir     : $cashier\n";
+    //     $text .= "Member    : $member\n";
+    //     $text .= "--------------------------------\n";
+
+    //     $nameWidth = 16;
+    //     $qtyWidth = 5;
+    //     $priceWidth = 11;
+    //     $this->total_qty = 0;
+    //     $this->total_price = 0;
+
+    //     foreach ($this->order->orderProducts as $product) {
+    //         $productName = $product->product->product_name;
+    //         $quantity = $product->quantity;
+    //         $totalPrice = number_format($product->unit_price * $quantity);
+    //         $this->total_qty += $quantity;
+    //         $this->total_price += $product->unit_price * $product->quantity;
+    //         $diskon = number_format($this->order->discount_price);
+
+    //         $paddedName = str_pad($productName, $nameWidth);
+    //         $paddedQty = str_pad($quantity, $qtyWidth, ' ', STR_PAD_LEFT);
+    //         $paddedPrice = str_pad($totalPrice, $priceWidth, ' ', STR_PAD_LEFT);
+
+    //         $text .= "$paddedName$paddedQty$paddedPrice\n";
+    //     }
+    //     $textTotalBarang = str_pad("Total Barang", $nameWidth);
+    //     $totalBarang = str_pad($this->total_qty, $qtyWidth, ' ', STR_PAD_LEFT);
+
+    //     $text .= "--------------------------------\n";
+
+    //     $space = str_pad("", 3, ' ', STR_PAD_LEFT);
+    //     $spaceTotal = str_pad("", 2, ' ', STR_PAD_LEFT);
+    //     $textSubtotal = str_pad("Subtotal " . $this->total_qty . " Produk", 18);
+    //     $subtotal = str_pad(number_format($this->total_price), 12, ' ', STR_PAD_LEFT);
+    //     $text .= "$textSubtotal$spaceTotal$subtotal\n";
+
+    //     $textDiskon = str_pad("Diskon", 17);
+    //     $diskon = str_pad($diskon, 12, ' ', STR_PAD_LEFT);
+    //     $text .= "$textDiskon$space$diskon\n";
+
+    //     $this->ppn = ceil($this->total_price * 0.05);
+    //     $textPpn = str_pad("PPN", 17);
+    //     $ppn = str_pad(number_format($this->ppn), 12, ' ', STR_PAD_LEFT);
+    //     $text .= "$textPpn$space$ppn\n";
+
+    //     $text .= "--------------------------------\n";
+
+    //     $textTotal = str_pad("Total", 17);
+    //     $total = str_pad(number_format($this->order->grand_total), 12, ' ', STR_PAD_LEFT);
+    //     $text .= "$textTotal$space$total\n";
+
+    //     $textPembayaran = str_pad("Pembayaran", 17);
+    //     $pembayaranValue = str_pad($pembayaran, 12, ' ', STR_PAD_LEFT);
+    //     $text .= "$textPembayaran$space$pembayaranValue\n";
+
+    //     $textBayar = str_pad("Bayar", 17);
+    //     $bayar = str_pad(number_format($bayar), 12, ' ', STR_PAD_LEFT);
+    //     $text .= "$textBayar$space$bayar\n";
+
+    //     $textKembali = str_pad("Kembali", 17);
+    //     $kembali = str_pad(number_format($kembali), 12, ' ', STR_PAD_LEFT);
+    //     $text .= "$textKembali$space$kembali\n";
+
+    //     $text .= "--------------------------------\n";
+    //     $text .= centerTextCash('Terima Kasih') . "\n";
+
+    //     $client = new Client();
+    //     $response = $client->post('https://api.printnode.com/printjobs', [
+    //         'auth' => ['lO8U8x_2Z9w7uMWwA-MlM47khgWOHk5lAtfRLVLfeaw', ''],
+    //         'json' => [
+    //             'printerId' => '73602875', // Ganti dengan ID printer yang sesuai
+    //             'title' => 'Receipt Print',
+    //             'contentType' => 'raw_base64',
+    //             'content' => base64_encode($text), // Menggunakan konten struk yang telah diformat
+    //             'source' => 'Laravel Application'
+    //         ]
+    //     ]);
+
+    //     if ($response->getStatusCode() == 201) {
+    //         session()->flash('message', 'Struk berhasil dicetak.');
+    //     } else {
+    //         session()->flash('error', 'Terjadi kesalahan saat mencetak struk.');
+    //     }
+
+    //     return redirect()->route('order');
+    // }
+
     public function pay_cash()
     {
         $this->order = Order::where('done_at', null)
@@ -97,115 +221,31 @@ class Payment extends Component
             'done_at' => now()
         ]);
 
-        $orderId = strtoupper($this->order->invoice_number);
-        $cashier = auth()->user()->name;
-        $member = $this->order->member->name ?? $this->order->member->phone;
-        $address = 'Jl. Binong Jati No.124';
-        $address2 = 'Kec.Batununggal, Bandung, 40275';
-        $phone = '0856-2401-0106';
-        $date = $this->order->created_at->format('Y-m-d H:i');
-        $pembayaran = $this->order->payment_method;
-        $bayar = $this->order->paid_amount;
-        $kembali = $this->order->return_amount;
+        return redirect()->route('order');
+    }
 
-        function centerTextCash($text, $width = 32) {
-            if (strlen($text) >= $width) {
-                return $text;
-            }
-            $padding = floor(($width - strlen($text)) / 2);
-            return str_repeat(' ', $padding) . $text;
-        }
+    public function pay_qris()
+    {
+        $this->order = Order::where('done_at', null)
+        ->with('orderProducts')
+        ->latest()
+        ->first();
 
-        // Format the receipt text
-        $text = centerTextCash('Merajut Asa Kita') . "\n";
-        $text .= centerTextCash($address) . "\n";
-        $text .= centerTextCash($address2) . "\n";
-        $text .= centerTextCash($phone) . "\n\n";
+        $paid_amount = $this->order->grand_total;
 
-        $text .= "Tanggal   : $date\n";
-        $text .= "Order ID  : $orderId\n";
-        $text .= "Kasir     : $cashier\n";
-        $text .= "Member    : $member\n";
-        $text .= "--------------------------------\n";
-
-        $nameWidth = 16;
-        $qtyWidth = 5;
-        $priceWidth = 11;
-        $this->total_qty = 0;
-        $this->total_price = 0;
-
-        foreach ($this->order->orderProducts as $product) {
-            $productName = $product->product->product_name;
-            $quantity = $product->quantity;
-            $totalPrice = number_format($product->unit_price * $quantity);
-            $this->total_qty += $quantity;
-            $this->total_price += $product->unit_price * $product->quantity;
-            $diskon = number_format($this->order->discount_price);
-
-            $paddedName = str_pad($productName, $nameWidth);
-            $paddedQty = str_pad($quantity, $qtyWidth, ' ', STR_PAD_LEFT);
-            $paddedPrice = str_pad($totalPrice, $priceWidth, ' ', STR_PAD_LEFT);
-
-            $text .= "$paddedName$paddedQty$paddedPrice\n";
-        }
-        $textTotalBarang = str_pad("Total Barang", $nameWidth);
-        $totalBarang = str_pad($this->total_qty, $qtyWidth, ' ', STR_PAD_LEFT);
-
-        $text .= "--------------------------------\n";
-
-        $space = str_pad("", 3, ' ', STR_PAD_LEFT);
-        $spaceTotal = str_pad("", 2, ' ', STR_PAD_LEFT);
-        $textSubtotal = str_pad("Subtotal " . $this->total_qty . " Produk", 18);
-        $subtotal = str_pad(number_format($this->total_price), 12, ' ', STR_PAD_LEFT);
-        $text .= "$textSubtotal$spaceTotal$subtotal\n";
-
-        $textDiskon = str_pad("Diskon", 17);
-        $diskon = str_pad($diskon, 12, ' ', STR_PAD_LEFT);
-        $text .= "$textDiskon$space$diskon\n";
-
-        $this->ppn = ceil($this->total_price * 0.05);
-        $textPpn = str_pad("PPN", 17);
-        $ppn = str_pad(number_format($this->ppn), 12, ' ', STR_PAD_LEFT);
-        $text .= "$textPpn$space$ppn\n";
-
-        $text .= "--------------------------------\n";
-
-        $textTotal = str_pad("Total", 17);
-        $total = str_pad(number_format($this->order->grand_total), 12, ' ', STR_PAD_LEFT);
-        $text .= "$textTotal$space$total\n";
-
-        $textPembayaran = str_pad("Pembayaran", 17);
-        $pembayaranValue = str_pad($pembayaran, 12, ' ', STR_PAD_LEFT);
-        $text .= "$textPembayaran$space$pembayaranValue\n";
-
-        $textBayar = str_pad("Bayar", 17);
-        $bayar = str_pad(number_format($bayar), 12, ' ', STR_PAD_LEFT);
-        $text .= "$textBayar$space$bayar\n";
-
-        $textKembali = str_pad("Kembali", 17);
-        $kembali = str_pad(number_format($kembali), 12, ' ', STR_PAD_LEFT);
-        $text .= "$textKembali$space$kembali\n";
-
-        $text .= "--------------------------------\n";
-        $text .= centerTextCash('Terima Kasih') . "\n";
-
-        $client = new Client();
-        $response = $client->post('https://api.printnode.com/printjobs', [
-            'auth' => ['lO8U8x_2Z9w7uMWwA-MlM47khgWOHk5lAtfRLVLfeaw', ''],
-            'json' => [
-                'printerId' => '73602875', // Ganti dengan ID printer yang sesuai
-                'title' => 'Receipt Print',
-                'contentType' => 'raw_base64',
-                'content' => base64_encode($text), // Menggunakan konten struk yang telah diformat
-                'source' => 'Laravel Application'
-            ]
+        $this->order->update([
+            'paid_amount' => $paid_amount,
+            'payment_method' => 'Qris',
+            'done_at' => now(),
+            'return_amount' => 0
         ]);
 
-        if ($response->getStatusCode() == 201) {
-            session()->flash('message', 'Struk berhasil dicetak.');
-        } else {
-            session()->flash('error', 'Terjadi kesalahan saat mencetak struk.');
-        }
+        foreach ($this->order->orderProducts as $itemProduct) {
+                $product = Product::find($itemProduct->product_id);
+                if ($product) {
+                    $product->decrement('stock', $itemProduct->quantity);
+                }
+            }
 
         return redirect()->route('order');
     }
@@ -335,32 +375,6 @@ class Payment extends Component
 
     //     return redirect()->route('order');
     // }
-
-    public function pay_qris()
-    {
-        $this->order = Order::where('done_at', null)
-        ->with('orderProducts')
-        ->latest()
-        ->first();
-
-        $paid_amount = $this->order->grand_total;
-
-        $this->order->update([
-            'paid_amount' => $paid_amount,
-            'payment_method' => 'Qris',
-            'done_at' => now(),
-            'return_amount' => 0
-        ]);
-
-        foreach ($this->order->orderProducts as $itemProduct) {
-                $product = Product::find($itemProduct->product_id);
-                if ($product) {
-                    $product->decrement('stock', $itemProduct->quantity);
-                }
-            }
-
-        return redirect()->route('order');
-    }
 
 
 }
--- resources/views/livewire/cart/payment.blade.php
+++ resources/views/livewire/cart/payment.blade.php
@@ -208,10 +208,12 @@
                         <span class="text-sm font-normal text-gray-800">Rp{{ number_format($return_amount, 0, ',', '.')
                             }}</span>
                     </div>
-                    <button wire:click="pay_cash"
-                        class="text-white inline-flex w-full justify-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Cetak Struk
-                    </button>
+                    <form action="{{ route('receipt') }}" method="GET" target="_blank">
+                        <button wire:click="pay_cash"
+                            class="text-white inline-flex w-full justify-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Cetak Struk
+                        </button>
+                    </form>
                     @endif
                 </div>
             </div>
